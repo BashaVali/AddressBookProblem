2f50bc6b87b592226c974121d9c38ab4713fc020
BashaVali <bashavali222@gmail.com>
2022-12-12 05:15:28 +0530

    Address Book Problem

---
+++ AddressBookProblem/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblem
+{
+    public class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipCode { get; set; }
+        public string phoneNumber { get; set; }
+        public string EmailId { get; set; }
+    }
+}
---
+++ AddressBookProblem/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblem
+{
+  
+    }
+    class AddressBook
+    {
+        public  void PersonDetails()
+        {
+            Console.Write("Enter First Name: ");
+            String firstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Enter Address : ");
+            var Addresses = Console.ReadLine();
+
+            Console.Write("Enter City : ");
+            string city = Console.ReadLine();
+
+            Console.Write("Enter State : ");
+            string state = Console.ReadLine();
+
+            Console.Write("Enter ZipCode: ");
+            string zipCode = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Enter EmailId: ");
+            string Email = Console.ReadLine();
+            //Adding fetched details to list
+            List<string> Contact = new List<string>();
+            Contact.Add(firstName);
+            Contact.Add(lastName);
+            Contact.Add(city);
+            Contact.Add(state);
+            Contact.Add(zipCode);
+             Contact.Add(phoneNumber);
+            Contact.Add(Email);
+            foreach (string i in Contact)
+                Console.WriteLine(i);
+
+        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book!");
+            AddressBook addressBook = new AddressBook();
+            addressBook.PersonDetails();
+        }
+    }
+}
